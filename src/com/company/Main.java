645571ecd6212ae85bfacc4cf9f1a120aceb86d0
Charles J Mitterer <cm921@pvhs-lab112-d09.pvsd.org>
2016-09-07 14:20:45 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.print("Hello World");
+        System.out.println("What is going on");
+        //THE ABOVE IS THE 2 WAYS WE NEED TO KNOW HOW TO out put text
+        //Two types of comments
+        // 1. line comment used with //
+        // 2. Block comment. That is done with a /*
+        System.out.print(1);
+        System.out.print(1.0);
+    }
+}
